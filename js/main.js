daad39843cacf5eacdad6230d19ab1b45de0685f
Marcos Paulo Badaró <marcospaulobadaro@gmail.com>
2022-07-28 17:57:36 -0300

    criando os estados da aplicação

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ const name = document.querySelector('#fname');
 const lname = document.querySelector('#lname');
 const email = document.querySelector('#email');
 const pword = document.querySelector('#pword');
+const form = document.querySelector('.singup-form');
 
 console.log(name);
 
 function send() {
-    
+   if(name.value = ""){
+    form.classList.add('.error')
+   }
 }
