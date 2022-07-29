2adaf77e6224ca7ee3865facf528a7d753e1f55a
Marcos Paulo Badaró <marcospaulobadaro@gmail.com>
2022-07-29 15:55:04 -0300

    Finalizando projeto

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
-const name = document.querySelector('#fname');
-const lname = document.querySelector('#lname');
-const email = document.querySelector('#email');
-const pword = document.querySelector('#pword');
+const name = document.querySelectorAll('#fname');
+const lname = document.querySelectorAll('#lname');
+const email = document.querySelectorAll('#email');
+const pword = document.querySelectorAll('#pword');
 const form = document.querySelector('.singup-form');
+const div = document.querySelector('.singup-form-input-area');
+const message = document.querySelector('.message');
+const input = document.querySelectorAll('input.singup-form-input');
 
-console.log(name);
-
-function send() {
-   if(name.value = ""){
-    form.classList.add('.error')
+function send(){ 
+   if(input.value = " "){
+      input.placeholder = " ";
+      form.classList.add('error');
+      name.forEach( (content) => { content.classList.add('error')})
    }
+   // if(lname.value = " "){
+   //    form.classList.add('error');
+   //    lname.forEach( (content) => { content.classList.add('error')})
+   // }
+   // if(email.value = " "){
+   //    form.classList.add('error');
+   //    email.forEach( (content) => { content.classList.add('error')})
+   // }
+   // if(pword.value = " "){
+   //    form.classList.add('error');
+   //    pword.forEach( (content) => { content.classList.add('error')})
+   // }
+
+   console.log(input)
+
 }
